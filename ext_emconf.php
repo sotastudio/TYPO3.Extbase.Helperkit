82fdd77c8a058eff857d2e243b4ba4f54fc26f30
Andy Hausmann <hi@andy-hausmann.de>
2013-06-16 18:40:54 +0200

    Added essentials for an Extension.

---
+++ ext_emconf.php
@@ -0,0 +1,50 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "fluidpages_bootstrap2".
+ *
+ * Auto generated 23-04-2013 23:27
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Collection of useful Helper for Fluid Templating and such.',
+	'description' => 'An extension providing a set of specially formatted Fluid templates usable as page templates enabled by EXT:fluidpages',
+	'category' => 'misc',
+	'shy' => 0,
+	'version' => '1.0.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Andreas Walter',
+	'author_email' => 'aw@sota-studio.de',
+	'author_company' => 'sota studio',
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '6.0-0.0.0',
+			'cms' => '',
+			'fluid' => '',
+		),
+		'conflicts' => '',
+		'suggests' => 
+		array (
+		),
+	),
+);
+
+?>
